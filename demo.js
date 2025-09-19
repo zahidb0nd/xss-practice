03b85948edbce8701fbfbf8873925e2944c57c69
zahidb0nd <zahidbond7@gmail.com>
2025-09-19 20:36:01 +0530

    express backend

---
+++ demo.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  const message = {
+    message: "Hello, World!",
+  };
+  res.send(message);
+});
+
+app.get("/reflectedxssone", (req, res) => {
+  const name = req.query.name || "guest ";
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head><Title>Reflected XSS Example</Title></head>
+        <body>
+            <h1>Welcome ${name}</h1>
+        </body>
+    </html>`;
+
+  res.send(htmlContent);
+});
+
+const PORT = process.env.PORT || 9999;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
